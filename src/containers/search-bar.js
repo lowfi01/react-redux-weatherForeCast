b041f5466be1d635b5f8d9d70ca54957c4520c54
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-09-03 19:16:59 +1000

    Created component level state & setup controlled form - state now is the value of input & input will display that value

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -3,16 +3,41 @@ import { Connect } from 'react-redux';
 
 
 export default class SearchBar extends Component{
+    //Create a controlled field by declaring a component level state
+    //This state will update on input & then render to the input field as it's value,
+    //This is in-essence the input value - But we can access it & in turn
+    //Declare & use it's value in arguments 
+
+    //Creating a component level state
+    constructor(props){
+      super(props);
+
+      // use this state for declaring & setting he input value
+      this.state = {term: ''}
+    }
+    
+
+    // DOM event handlers return an event
+    onInputChange(event) {
+        console.log(event.target.value);
+
+        //Update Component state with the event.target.value
+        //This will set the state to the input values entered by user
+        this.setState({term: event.target.value})
+    };
+
     render() {
         return (
             <div>
                 <form className="input-group">
-                    <input />
+                    <input  onChange={this.onInputChange.bind(this)} value={this.state.term} className="form-control" placeholder="Get a five-day forecast in your favorite cities"/>
                     <span className="input-group-btn">
                         <button type="submit" className="btn btn-secondary">Submit</ button> 
                     </span>
                 </form>
             </div>
         )
-    }
+    };
+
+
 }
