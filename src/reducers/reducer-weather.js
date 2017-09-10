0304c7b35739a7d41ac06aa6fa299282eacd3e88
DESKTOP-85TAR51\james <jamesherrero@gmail.com>
2017-09-10 19:02:23 +1000

    Completed reducer to accept payload & add to application state

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,6 +1,27 @@
 
 
-export default function(state = null, action){
-    console.log('Action Received', action)
+//import action type
+import {FETCH_WEATHER} from './../actions/index';
+
+//we would like to have a state with all of our
+//weather data available for a 5 day period
+//the best approach would be to make an array
+//the first action - should then pull all of the data we require
+//from the API & store it into application state -
+//this would also ensure we are only calling the API once & reducing load times
+
+export default function(state = [], action){
+    // create switch statement
+    switch(action.type){
+        //FETCH_WEATHER only when searching for a city
+        case FETCH_WEATHER:
+        //Add Fetched data to the state
+        // Note - First part is pushed to top of the array
+        return [ action.payload.data, ...state ];
+        // alternative code
+        //return state.concat([action.payload.data]);
+        
+
+    }
     return state
 }
